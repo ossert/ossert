b917153786ba17ae443eba989abdfa230479095d
K. Volchenko <likeath@gmail.com>
2016-12-05 23:04:40 +0300

    Feature: add full text search index for projects

---
+++ db/migrate/006_add_search_index_to_projects.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+Sequel.migration do
+  up do
+    run 'CREATE EXTENSION IF NOT EXISTS pg_trgm'
+    run 'CREATE INDEX projects_name_search_index ON projects USING gist (name gist_trgm_ops)'
+  end
+
+  down do
+    run 'DROP INDEX projects_name_search_index'
+  end
+end
